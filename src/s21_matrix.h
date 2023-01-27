1c9ec21d316cc9f45d5c6091c7ddf85f44f6797d
Darina Ayurova <comedial@student.21-school.ru>
2023-01-27 12:07:26 +0300

    q

---
+++ src/s21_matrix.h
@@ -0,0 +1,30 @@
+#ifndef SRC_S21_MATRIX_H_
+#define SRC_S21_MATRIX_H_
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+enum { OK, INCORRECT, CALC_ERROR };
+
+#define SUCCESS 1
+#define FAILURE 0
+
+typedef struct matrix_struct {
+  double **matrix;
+  int rows;
+  int columns;
+} matrix_t;
+
+int s21_create_matrix(int rows, int columns, matrix_t *result);
+void s21_remove_matrix(matrix_t *A);
+int s21_eq_matrix(matrix_t *A, matrix_t *B);
+int s21_sum_matrix(matrix_t *A, matrix_t *B, matrix_t *result);
+int s21_sub_matrix(matrix_t *A, matrix_t *B, matrix_t *result);
+int s21_mult_number(matrix_t *A, double number, matrix_t *result);
+
+// sup funcs
+int correct_matrix(matrix_t *A);
+int equal_size(matrix_t *A, matrix_t *B);
+
+#endif // SRC_S21_MATRIX_H_
